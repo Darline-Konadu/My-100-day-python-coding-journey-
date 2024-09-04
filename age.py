5ab821aa0dc83b9d16e7cb2c4860fca371a88271
Darline-Konadu <darlineamoafo@gmail.com>
2024-09-04 20:15:16 +0000

    3
    Continuation of day 2

---
+++ age.py
@@ -0,0 +1,11 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+age = input("what is your current age?")
+age = int(age)
+years_left = 90-age
+days_left = years_left * 365
+weeks_left = years_left * 52
+months_left = years_left *12
+
+print(f"You have {years_left} years,{days_left} days,{weeks_left} weeks and {months_left} months")
---
+++ bmi.py
@@ -0,0 +1,5 @@
+height = input("Enter your height in m: ")
+weight = input("Enter your weight in kg: ")
+bmi = float(weight)/float(height)**2
+answer = int(bmi)
+print(answer)
