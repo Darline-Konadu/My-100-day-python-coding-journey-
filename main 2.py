a0d9fdbf3646c7a1bb4633ed6b56d69feed4e1fb
Darline-Konadu <darlineamoafo@gmail.com>
2024-09-04 12:36:01 +0000

    Add files via upload
    Day1

---
+++ main 2.py
@@ -0,0 +1,6 @@
+
+two_digit_number = input("Type a two digit number: ")
+first_number = two_digit_number[0]
+second_number = two_digit_number[1]
+answer = int(first_number)+ int(second_number)
+print(answer)
